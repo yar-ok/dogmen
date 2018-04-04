7d2b6e71138013673003649b2dc4e214e3abbdd2
Yaroslav Katrenko <yaroslav.katrenko@gmail.com>
2018-04-04 15:37:28 +0300

    added find location

--- app/containers/MapScreen.js
+++ app/containers/MapScreen.js
@@ -2,13 +2,95 @@ import React, { Component } from "react";
 
 import { StyleSheet, View } from "react-native";
 
+import AppHeaderTitle from "../components/AppHeaderTitle";
+import Resources from "../utils/Resources";
+
 import MapView from "react-native-maps";
 
 class MapScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            markers: [
+                {
+                    coordinate: {
+                        latitude: 49.437710,
+                        longitude: 32.085894,
+                    },
+                },
+                {
+                    coordinate: {
+                        latitude: 49.445641,
+                        longitude: 32.092245,           
+                    },
+                },
+                {
+                    coordinate: {
+                        latitude: 49.449239, 
+                        longitude: 32.068127,           
+                    },
+                },
+                {
+                    coordinate: {
+                        latitude: 49.420246, 
+                        longitude: 32.058142,           
+                    },
+                }
+            ],
+            myLocation: {
+                latitude: 49.408073, 
+                longitude: 32.044204,
+                latitudeDelta: 0.04864195044303443,
+                longitudeDelta: 0.040142817690068
+            }
+        }
+    }
+
+    static navigationOptions = ({ navigation }) => {
+        const { params = {} } = navigation.state;
+        return {
+        headerTitle: <AppHeaderTitle title="Location" />,
+        headerTintColor: "white",
+        headerRight: <View />,
+        headerStyle: {
+            backgroundColor: Resources.TOOLBAR_COLOR
+        }
+        };
+    };
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    myLocation: {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                        latitudeDelta: 0.04864195044303443,
+                        longitudeDelta: 0.040142817690068
+                    } 
+                });
+            },
+            (error) => alert('Location error -> ' + error.message),
+            { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 },
+        );
+    }
+
     render() {
-        return <View>
-            <MapView style={ styles.container } region={{ latitude: 37.78825, longitude: -122.4324, latitudeDelta: 0.015, longitudeDelta: 0.0121 }} />
-          </View>;
+        return(
+            <View>
+                <MapView style={ styles.container } 
+                showsUserLocation={true}
+                region={this.state.myLocation}
+                initialRegion={{ 
+                    latitude: this.state.myLocation.latitude, 
+                    longitude: this.state.myLocation.longitude, 
+                    latitudeDelta: this.state.myLocation.latitudeDelta, 
+                    longitudeDelta: this.state.myLocation.longitudeDelta 
+                }} 
+                
+                />
+            </View>
+        )
     }
 }
 
@@ -18,5 +100,6 @@ const styles = StyleSheet.create({
     width: "100%"
   }
 });
+                // region={{ latitude: this.state.latitude, longitude: this.state.longitude, latitudeDelta: this.state.latitudeDelta, longitudeDelta: this.state.longitudeDelta }}
 
 export default MapScreen
