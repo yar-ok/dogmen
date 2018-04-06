06d72aecc61df2fff173de6f17ed5b41ca7cdde3
Yaroslav Katrenko <yaroslav.katrenko@gmail.com>
2018-04-06 10:36:16 +0300

    finished with AddLocation screen

--- app/containers/AddLocation.js
+++ app/containers/AddLocation.js
@@ -9,6 +9,9 @@ import {
   Image
 } from "react-native";
 
+import AppHeaderTitle from "../components/AppHeaderTitle";
+import Resources from "../utils/Resources";
+
 const homePlace = {
   description: "Home",
   geometry: { location: { lat: 48.8152937, lng: 2.4597668 } }
@@ -19,48 +22,61 @@ const workPlace = {
 };
 
 class AddLocation extends Component {
-    saveCoordinatesAndExit(coordinates) {
-      
-    }
+  static navigationOptions = ({ navigation }) => {
+    const { params = {} } = navigation.state;
+    return {
+      headerTitle: <AppHeaderTitle title="Location" />,
+      headerTintColor: "white",
+      headerRight: <View />,
+      headerStyle: {
+        backgroundColor: Resources.TOOLBAR_COLOR
+      }
+    };
+  };
 
-    render() {
-        return(
-        <GooglePlacesAutocomplete
-        placeholder='Enter Location'
+  saveCoordinatesAndExit(coordinates) {}
+
+  render() {
+    return (
+      <GooglePlacesAutocomplete
+        placeholder="Enter Location"
         minLength={2}
         autoFocus={false}
-        returnKeyType={'default'}
+        returnKeyType={"default"}
         fetchDetails={true}
         query={{
-            // available options: https://developers.google.com/places/web-service/autocomplete
-            key: 'AIzaSyA90xwRzF8BSN11MOEH8FrnF6hU3vEkNJY',
-            language: 'en',
+          // available options: https://developers.google.com/places/web-service/autocomplete
+          key: "AIzaSyA90xwRzF8BSN11MOEH8FrnF6hU3vEkNJY",
+          language: "en"
         }}
         onPress={(data, details = null) => {
-            coordinates = { latitude: details.geometry.location.lat, longitude: details.geometry.location.lng }
-            this.saveCoordinatesAndExit(coordinates)
+          coordinates = {
+            latitude: details.geometry.location.lat,
+            longitude: details.geometry.location.lng
+          };
+          this.saveCoordinatesAndExit(coordinates);
         }}
         styles={{
-            textInputContainer: {
-            backgroundColor: 'rgba(0,0,0,0)',
+          textInputContainer: {
+            backgroundColor: "rgba(0,0,0,0)",
             borderTopWidth: 0,
-            borderBottomWidth:0
-            },
-            textInput: {
+            borderBottomWidth: 0
+          },
+          textInput: {
             marginLeft: 0,
             marginRight: 0,
             height: 38,
-            color: '#5d5d5d',
+            color: "#5d5d5d",
             fontSize: 16
-            },
-            predefinedPlacesDescription: {
-            color: '#1faadb'
-            },
+          },
+          predefinedPlacesDescription: {
+            color: "#1faadb"
+          }
         }}
         currentLocation={false}
-        />
-        )
-    }
+      />
+    );
+  }
 }
 
 export default AddLocation
