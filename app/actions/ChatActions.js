39dbf588d5812c4a7d47caeab30ea9210c691734
Yaroslav Katrenko <yaroslav.katrenko@gmail.com>
2018-04-18 15:46:30 +0300

    working with firebase database

--- app/actions/ChatActions.js
+++ app/actions/ChatActions.js
@@ -11,97 +11,104 @@ export const actionCreators = {
         error: false
       }
     });
-    setTimeout (() => {
-      // check for server error
-      if (true) {
-        let messages = [
-            {
-                id: '7',
-                message: 'I think',
-                user: {
-                    name: 'Bob(me)',
-                    avatar: 'https://randomuser.me/api/portraits/thumb/men/54.jpg',
-                    isMe: true,
-                }
-            },
-           {
-                id: '6',
-                message: 'No problem!',
-                user: {
-                    name: 'Bob(me)',
-                    avatar: 'https://randomuser.me/api/portraits/thumb/men/54.jpg',
-                    isMe: true,
-                }
-            },
-            {
-                id: '5',
-                message: 'Yes!!!',
-                user: {
-                    name: 'Bob(me)',
-                    avatar: 'https://randomuser.me/api/portraits/thumb/men/54.jpg',
-                    isMe: true,
-                }
-            },
+    let url = "https://dogmen-5b44f.firebaseio.com/messages";
+    
+    // fetch(url)
+    //   .then(res => res.json())
+    //   .then(res => alert('OK'))
 
-            {
-                id: '4',
-                message: 'I want to tell you something)))',
-                user: {
-                    name: 'Kelly',
-                    avatar: 'https://randomuser.me/api/portraits/thumb/women/63.jpg',
-                    isMe: false,
-                }
-            },
-            
-          {
-                id: '3',
-                message: 'How are you?',
-                user: {
-                    name: 'Kelly',
-                    avatar: 'https://randomuser.me/api/portraits/thumb/women/63.jpg',
-                    isMe: false,
-                }
-            },
-            
-            {
-                id: '2',
-                message: 'Hi!!!',
-                user: {
-                    name: 'Bob(me)',
-                    avatar: 'https://randomuser.me/api/portraits/thumb/men/54.jpg',
-                    isMe: true,
-                }
-            },
+    
+    // setTimeout (() => {
+    //   // check for server error
+    //   if (true) {
+    //     let messages = [
+    //         {
+    //             id: '7',
+    //             message: 'I think',
+    //             user: {
+    //                 name: 'Bob(me)',
+    //                 avatar: 'https://randomuser.me/api/portraits/thumb/men/54.jpg',
+    //                 isMe: true,
+    //             }
+    //         },
+    //        {
+    //             id: '6',
+    //             message: 'No problem!',
+    //             user: {
+    //                 name: 'Bob(me)',
+    //                 avatar: 'https://randomuser.me/api/portraits/thumb/men/54.jpg',
+    //                 isMe: true,
+    //             }
+    //         },
+    //         {
+    //             id: '5',
+    //             message: 'Yes!!!',
+    //             user: {
+    //                 name: 'Bob(me)',
+    //                 avatar: 'https://randomuser.me/api/portraits/thumb/men/54.jpg',
+    //                 isMe: true,
+    //             }
+    //         },
 
-            {
-                id: '1',
-                message: 'Hello',
-                user: {
-                    name: 'Kelly',
-                    avatar: 'https://randomuser.me/api/portraits/thumb/women/63.jpg',
-                    isMe: false,
-                }
-            },
-           ]
-        dispatch({
-          type: types.ALL_CHAT_MESSAGES,
-          payload: {
-            loading: false,
-            error: false,
-            result: handleMassages(messages)
-          }
-        });
-      } else {
-        //error
-        dispatch({
-          type: types.ALL_CHAT_MESSAGES,
-          payload: {
-            loading: false,
-            error: true
-          }
-        });
-      }
-    }, 2000)
+    //         {
+    //             id: '4',
+    //             message: 'I want to tell you something)))',
+    //             user: {
+    //                 name: 'Kelly',
+    //                 avatar: 'https://randomuser.me/api/portraits/thumb/women/63.jpg',
+    //                 isMe: false,
+    //             }
+    //         },
+            
+    //       {
+    //             id: '3',
+    //             message: 'How are you?',
+    //             user: {
+    //                 name: 'Kelly',
+    //                 avatar: 'https://randomuser.me/api/portraits/thumb/women/63.jpg',
+    //                 isMe: false,
+    //             }
+    //         },
+            
+    //         {
+    //             id: '2',
+    //             message: 'Hi!!!',
+    //             user: {
+    //                 name: 'Bob(me)',
+    //                 avatar: 'https://randomuser.me/api/portraits/thumb/men/54.jpg',
+    //                 isMe: true,
+    //             }
+    //         },
+
+    //         {
+    //             id: '1',
+    //             message: 'Hello',
+    //             user: {
+    //                 name: 'Kelly',
+    //                 avatar: 'https://randomuser.me/api/portraits/thumb/women/63.jpg',
+    //                 isMe: false,
+    //             }
+    //         },
+    //        ]
+    //     dispatch({
+    //       type: types.ALL_CHAT_MESSAGES,
+    //       payload: {
+    //         loading: false,
+    //         error: false,
+    //         result: handleMassages(messages)
+    //       }
+    //     });
+    //   } else {
+    //     //error
+    //     dispatch({
+    //       type: types.ALL_CHAT_MESSAGES,
+    //       payload: {
+    //         loading: false,
+    //         error: true
+    //       }
+    //     });
+    //   }
+    // }, 2000)
   },
 
   sendNewMessage: (message, messages) => async(dispatch, getState) => {
---
+++ app/config/firebase.js
@@ -0,0 +1,7 @@
+import * as firebase from "firebase";
+
+const config = {
+  // your config stuff
+};
+firebase.initializeApp(config);
+export default firebase;
