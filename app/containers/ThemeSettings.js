44faf3fdaa9e46ec2ae058682164ddbd364291b4
Yaroslav Katrenko <yaroslav.katrenko@gmail.com>
2018-05-09 14:39:18 +0300

    updated app theme screen

--- app/containers/ThemeSettings.js
+++ app/containers/ThemeSettings.js
@@ -10,6 +10,9 @@ import { actionCreators } from '../actions/SettingsActions'
 import { connect } from 'react-redux'
 import Constants from '../utils/Constants'
 
+import AppHeaderTitle from "../components/AppHeaderTitle";
+import { store } from "../config/store";
+
 const mapStateToProps = (state) => ({
     // loading: state.loginState.loading,
     // token: state.loginState.token,
@@ -23,12 +26,24 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 class ThemeSettings extends Component {
+    static navigationOptions = ({ navigation }) => {
+        const { params = {} } = navigation.state;
+        return {
+            headerTitle: <AppHeaderTitle title="Theme Settings" />,
+            headerTintColor: "white",
+            headerRight: <View />,
+            headerStyle: {
+                backgroundColor: store.getState().settingsState.toolbar_color
+            }
+        };
+    };
+
     render() {
         return(
             <View>
                 <Button
                 title="Click"
-                onPress={() => this.props.changeTheme(Constants.RED_THEME)} />
+                onPress={() => this.props.changeTheme(Constants.GREEN_THEME)} />
             </View>
         )
     }
